8fa3e425fb45a835f733d669fc5173f99ef0026a
Irina Burnina <irinkaburninka@gmail.com>
2023-01-22 10:34:17 +0300

    Test verify value add to MedicalServiceImplTest

--- src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
@@ -1,8 +1,11 @@
 package ru.netology.patient.service.medical;
 
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.BloodPressure;
 import ru.netology.patient.entity.HealthInfo;
@@ -25,7 +28,6 @@ public class MedicalServiceImplTest {
         SendAlertServiceImpl alertService = Mockito.mock(SendAlertServiceImpl.class);
         MedicalServiceImpl medicalService = new MedicalServiceImpl(patientInfoFileRepository, alertService);
         medicalService.checkBloodPressure(patientInfo.getId(), bloodPressure);
-
         Mockito.verify(alertService, Mockito.times(0)).send(message);
     }
 
@@ -59,7 +61,20 @@ public class MedicalServiceImplTest {
                 LocalDate.of(1982, 01, 16), new HealthInfo(new BigDecimal(36.6),
                 new BloodPressure(125, 42))), new BloodPressure(100, 50)));
     }
-
+    @ParameterizedTest
+    @MethodSource("sourceMethodForCheckBloodPressureTestSend")
+    public void checkBloodPressureTestSendValue(PatientInfo patientInfo, BloodPressure bloodPressure) {
+        PatientInfoFileRepository patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
+        Mockito.when(patientInfoFileRepository.getById(patientInfo.getId()))
+                .thenReturn(patientInfo);
+        SendAlertServiceImpl alertService = Mockito.mock(SendAlertServiceImpl.class);
+        MedicalServiceImpl medicalService = new MedicalServiceImpl(patientInfoFileRepository, alertService);
+        medicalService.checkBloodPressure(patientInfo.getId(), bloodPressure);
+        String message = String.format("Warning, patient with id: %s, need help", patientInfo.getId());
+        ArgumentCaptor<String> argumentCaptor=ArgumentCaptor.forClass(String.class);
+        Mockito.verify(alertService).send(argumentCaptor.capture());
+        Assertions.assertEquals(message,argumentCaptor.getValue());
+        }
     @ParameterizedTest
     @MethodSource("sourceMethodForCheckNormalTemperatureTestNoneSend")
     public void checkNormalTemperatureTestNoneSend(PatientInfo patientInfo, BigDecimal temperature) {
